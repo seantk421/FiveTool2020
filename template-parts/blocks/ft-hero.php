290bc5a8619a17ba241567b59d045ced6e0f27c0
Sean Carnahan <sean.carnahan@5toolmarketing.com>
2020-01-28 13:16:17 -0500

    updating with hero background

--- template-parts/blocks/ft-hero.php
+++ template-parts/blocks/ft-hero.php
@@ -1,12 +1,15 @@
 <?php 
+// Default block classes
 $blockClass = 'ft-hero ft-block';
 
 // Define the hero layout  
 // Add hero layout to the block wrapping class for custom styling.
-$hero_layout = get_sub_field('ft_hero_layout');
-if($hero_layout) {
+if($hero_layout = get_sub_field('ft_hero_layout')) {
     $blockClass .= ' ' . $hero_layout;
 }
+
+$styleString = '';
+// TODO: Check for advanced block options
 
 if($hero_layout === 'ft-hero-st') {
     
@@ -15,8 +18,19 @@ if($hero_layout === 'ft-hero-st') {
     // *******
     if( have_rows('ft_hero_standard_gr') ):
         while( have_rows('ft_hero_standard_gr') ): the_row(); ?>
+            <?php 
+                
+                // Add the background image specified at the block level
+                if($backgroundImage = get_sub_field('ft_hero_bkg_img')) {
+                    if($styleString == '') {
+                        $styleString .= ' style="background-image: URL(' . $backgroundImage['url'] . ')"';
+                    }
+                }
 
-            <section class="<?php echo $blockClass ?>">
+            ?>
+
+            <!-- FT Standard Hero -->
+            <section class="<?php echo $blockClass ?> bkg-cover" <?php echo $styleString ?>>
                 <div class="container">
                     <?php if($heroContent = get_sub_field('ft_hero_content')) {
                         echo $heroContent;
