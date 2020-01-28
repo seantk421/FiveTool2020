d2bfbdeb7bfc2040a9efab1d9688c957122f6b33
Sean Carnahan <sean.carnahan@5toolmarketing.com>
2020-01-28 13:04:49 -0500

    finalizing initial theme setup, adding nav mobile support, adding overrides, organizing nav js files

--- inc/customizer.php
+++ inc/customizer.php
@@ -50,6 +50,6 @@ function fivetoolagency_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function fivetoolagency_customize_preview_js() {
-	wp_enqueue_script( 'fivetoolagency-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+	wp_enqueue_script( 'fivetoolagency-customizer', get_template_directory_uri() . '/src/js/global/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'fivetoolagency_customize_preview_js' );
